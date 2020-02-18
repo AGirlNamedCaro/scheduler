5b871b66c6e41c5656c9d4f8d1bb8d829fbe5af5
Carolina Contreras Issa <agirlnamedcaro@gmail.com>
2020-02-18 14:44:56 -0700

    added a second state to days and passed it the array from the server

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,31 +1,22 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import "components/Application.scss";
 import DayList from "components/DayList";
 import 'components/Appointment';
 import Appointment from "./Appointment";
+import axios from 'axios';
 
-//TODO MERGE BRANCH TO MASTER
 
 export default function Application(props) {
   //This line adds 'STATE' to your application
   const [day, setDay] = useState("Monday");
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ];
+  const [days, setDays] = useState([]);
+  
+  useEffect(() => {
+    axios.get('http://localhost:8001/api/days')
+    .then((response) => {
+      setDays(response.data)
+    })
+  })
 
   const appointments = [
     {
--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,16 +3,17 @@ import DayListItem from 'components/DayListItem';
 // import days from 'components/days';
 
 export default function DayList(props) {
-   const list = props.days.map(day => {
-    return (
-    <DayListItem 
-      name={day.name}
-      spots={day.spots}
-      selected={day.name === props.day}
-      setDay={props.setDay}
-    />
-    );
-  });
+  // console.log("props from DayList", props);
+   const list = props.days.map(day => (
+      <DayListItem 
+        key={day.id}
+        name={day.name}
+        spots={day.spots}
+        selected={day.name === props.day}
+        setDay={props.setDay}
+      />
+    )
+  )
 
   return (
     <ul>
