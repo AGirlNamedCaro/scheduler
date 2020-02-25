d4291d35d4ae6a450ae42568896f02fc2a71b3e5
Carolina Contreras Issa <agirlnamedcaro@gmail.com>
2020-02-25 13:47:58 -0700

    Implemented the edit and delete tests on cypress

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -16,9 +16,49 @@ describe("Appointments", () => {
     cy.get('[alt="Sylvia Palmer"]')
       .click();
 
-      cy.contains("Save")
+    cy.contains("Save")
         .click();
+
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
+  })
+
+  it("should edit an interview", () => {
+    cy.get('.appointment__card')
+      .click();
+
+    cy.get('[alt="Edit"]')
+      .first()
+      .click({force: true});
     
-     
+    cy.get('[data-testid=student-name-input]')
+      .clear()
+      .type("Caro");
+
+    cy.get('[alt="Tori Malcolm"]')
+      .click();
+    
+      cy.contains("Save")
+      .click();
+
+    cy.contains(".appointment__card--show", "Caro");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+    
+  })
+
+  it('should cancel an interview', () => {
+    cy.get('.appointment__card')
+      .click();
+
+    cy.get('[alt="Delete"]')
+      .first()
+      .click({force: true});
+    
+    cy.contains("Confirm")
+      .click();
+
+    cy.contains(".appointment__card--show", "Archie Cohen").should("not.exist");
+    
+    
   })
 })
